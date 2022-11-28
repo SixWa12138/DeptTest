4fd8f2270fcd417e3c69397d121e1dbacd42cc02
tangyh <tangyihan_0712@163.com>
2022-11-28 18:52:05 +0800

    创建dao实现类

---
+++ src/com/sm/util/dao/impl/DeptDaoImpl.java
@@ -0,0 +1,44 @@
+package com.sm.util.dao.impl;
+
+import com.sm.entry.Dept;
+import com.sm.util.dao.IDeptDao;
+
+import java.util.List;
+
+/**
+ * @Auther:唐一涵
+ * @Date:2022/11/28
+ * @Description: com.sm.util.dao.impl
+ * @version: 1.0
+ */
+public class DeptDaoImpl implements IDeptDao {
+    @Override
+    public Dept query(Dept dept) {
+        return null;
+    }
+
+    @Override
+    public List<Dept> queryDept() {
+        return null;
+    }
+
+    @Override
+    public int add(Dept dept) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Dept dept) {
+        return 0;
+    }
+
+    @Override
+    public int update(Dept dept) {
+        return 0;
+    }
+
+    @Override
+    public int uniMethod(String sql, Object... objs) {
+        return 0;
+    }
+}
